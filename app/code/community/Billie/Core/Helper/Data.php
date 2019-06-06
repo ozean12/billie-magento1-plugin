eb519f96b7a6a7e35146278ad15177822aa06a98
Niels Gongoll <ngongoll@highdigital.de>
2019-06-06 11:12:42 +0200

    Branding frontend salutation camparision for default checked

--- app/code/community/Billie/Core/Helper/Data.php
+++ app/code/community/Billie/Core/Helper/Data.php
@@ -27,10 +27,40 @@ class Billie_Core_Helper_Data extends Mage_Core_Helper_Abstract {
 
     }
 
+    /**
+     *
+     *
+     * @return mixed
+     *
+     */
 
     public function getCompany(){
 
         return $this->htmlEscape(Mage::getModel('checkout/cart')->getQuote()->getBillingAddress()->getCompany());
+
+    }
+
+    public function compareSalutation($option){
+
+        $salutation = strtolower(Mage::getModel('checkout/cart')->getQuote()->getBillingAddress()->getPrefix());
+        $maleComparison = array('herr','male','mann','mister');
+        $femaleComparison = array('frau','female','miss');
+        $selectHtml = '';
+
+        if(in_array($salutation,$maleComparison) && $option = 'male'){
+
+            $selectHtml = 'checked="checked"';
+
+        } else if(in_array($salutation,$femaleComparison) && $option = 'female'){
+
+            $selectHtml = 'checked="checked"';
+
+        }
+
+
+
+        return $selectHtml;
+
 
     }
 }
